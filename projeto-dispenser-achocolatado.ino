654b6a9c268985304098ec03add216574e404d55
GabrielLimaChagas <chagas.gabriel@pucpr.edu.br>
2025-04-29 02:11:02 -0300

    feat: servo motor test

--- projeto-dispenser-achocolatado.ino
+++ projeto-dispenser-achocolatado.ino
@@ -3,35 +3,19 @@
 Servo servoMotor;
 
 const int pinServo = 13;
-const int pinIRSensor = 14;
 
-int servoPos = 90;
-
-void handleDispenseCup() {
-  for (int i = 70; i < 120; i++) {
-    servoPos = i;
-    servoMotor.write(servoPos);
-    delay(10);
-  }
-  for (int i = 70; i < 70; i--) {
-    servoPos = i;
-    servoMotor.write(servoPos);
-    delay(10);
-  }
-  servoPos = 70;
-  servoMotor.write(servoPos);
-}
+int servoPos = 0;
 
 void setup() {
-  pinMode(pinIRSensor, INPUT);
-
   servoMotor.setPeriodHertz(50);
   servoMotor.attach(pinServo, 500, 2400);
   servoMotor.write(servoPos);
 }
 
 void loop() {
-  if (digitalRead(pinIRSensor) == LOW) {
-    handleDispenseCup();
+  while (servoPos < 180) {
+    servoMotor.write(servoPos);
+    servoPos++;
+    delay(100);
   }
 }
