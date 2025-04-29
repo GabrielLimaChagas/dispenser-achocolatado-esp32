10fef8aeac1659ec60b39176945f16ab1894c0f5
GabrielLimaChagas <chagas.gabriel@pucpr.edu.br>
2025-04-29 02:05:02 -0300

    feat: IR sensor test

--- projeto-dispenser-achocolatado.ino
+++ projeto-dispenser-achocolatado.ino
@@ -1,37 +1,12 @@
-#include <ESP32Servo.h>
-
-Servo servoMotor;
-
-const int pinServo = 13;
 const int pinIRSensor = 14;
 
-int servoPos = 90;
-
-void handleDispenseCup() {
-  for (int i = 70; i < 120; i++) {
-    servoPos = i;
-    servoMotor.write(servoPos);
-    delay(10);
-  }
-  for (int i = 70; i < 70; i--) {
-    servoPos = i;
-    servoMotor.write(servoPos);
-    delay(10);
-  }
-  servoPos = 70;
-  servoMotor.write(servoPos);
-}
-
 void setup() {
+  Serial.begin(115200);
   pinMode(pinIRSensor, INPUT);
-
-  servoMotor.setPeriodHertz(50);
-  servoMotor.attach(pinServo, 500, 2400);
-  servoMotor.write(servoPos);
 }
 
 void loop() {
   if (digitalRead(pinIRSensor) == LOW) {
-    handleDispenseCup();
+    Serial.println("Detectado");
   }
 }
