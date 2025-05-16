7cfa3c74f636ab75b508f9be6056102a036ec33a
GabrielLimaChagas <chagas.gabriel@pucpr.edu.br>
2025-05-15 23:20:08 -0300

    Update projeto-dispenser-achocolatado.ino
    feat: full proccess of making a drink

--- projeto-dispenser-achocolatado.ino
+++ projeto-dispenser-achocolatado.ino
@@ -1,146 +1,159 @@
-#include <WiFi.h>
-#include <WebServer.h>
 #include <ESP32Servo.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// const char* ssid = "TP-LINK_6CBAC2";
-// const char* password = "99915933";
-const char* ssid = "Wifi Gabriel L";
-const char* password = "senhateste";
+// Display LCD
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-WebServer server(80);
-Servo servoMotor;
-LiquidCrystal_I2C lcd(0x27, 16, 2); // Endereço comum: 0x27 ou 0x3F
+// Pinos dos componentes
+#define SERVO_COPO 13
+#define SENSOR_COPO 23
+#define SERVO_CHOCOLATE 25
+#define SERVO_LEITE 26
+#define SERVO_VALVULA 27
+#define MIXER 33
+#define BOTAO_CIMA 14
+#define BOTAO_OK 12
 
-const int pinServo = 13;
-const int pinDispenseButton = 14;
+#define TEMPO_DE_MISTURA 5
 
+// Objetos servo
+Servo servoCopo, servoChocolate, servoLeite, servoValvula;
 
-int servoPos = 90;
-
-const char index_html[] = R"rawliteral(
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Controle de Servo</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <style>
-    body { font-family: Arial; text-align: center; padding: 40px; }
-    input[type=range] { width: 80%%; }
-    .slider-container { margin-bottom: 40px; }
-  </style>
-</head>
-<body>
-  <h2>Controle do Servo Motor</h2>
-  <div class="slider-container">
-    <p>Ângulo: <span id="servoValue">90</span>°</p>
-    <input type="range" min="0" max="180" value="90" id="servoSlider">
-  </div>
-
-  <h2>Dispensar Copo</h2>
-  <div class="slider-container">
-    <input type="button" id="dispenseCup" value="Dispensar">
-  </div>
-
-  <script>
-    const servoSlider = document.getElementById("servoSlider");
-    const servoValue = document.getElementById("servoValue");
-    const dispenseCup = document.getElementById("dispenseCup")
-
-    servoSlider.oninput = function() {
-      servoValue.innerHTML = this.value;
-      fetch("/servo?value=" + this.value);
-    }
-
-    dispenseCup.onclick = function() {
-      fetch("/dispenseCup")
-    }
-  </script>
-</body>
-</html>
-)rawliteral";
-
-void handleRoot() {
-  server.send(200, "text/html", index_html);
-}
-
-void handleServo() {
-  if (server.hasArg("value")) {
-    servoPos = server.arg("value").toInt();
-    servoMotor.write(servoPos);
-  }
-  server.send(200, "text/plain", "OK");
-}
-
-void handleDispenseCup() {
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Dispensando...");
-  for (int i = 70; i < 120; i++) {
-    servoPos = i;
-    servoMotor.write(servoPos);
-    delay(10);
-  }
-  for (int i = 70; i < 70; i--) {
-    servoPos = i;
-    servoMotor.write(servoPos);
-    delay(10);
-  }
-  servoPos = 70;
-  servoMotor.write(servoPos);
-  server.send(200, "text/plain", "OK");
-
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Copo pronto!");
-  delay(1500);
-  updateLCD();
-}
-
-void updateLCD() {
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Servo: ");
-  lcd.print(servoPos);
-}
+// Estado do sistema
+int intensidade = 1; // 1=fraco, 2=médio, 3=forte
+String intensidades[] = {"Fraco", "Médio", "Forte"};
 
 void setup() {
   Serial.begin(115200);
-  Wire.begin();
+  
+  // Servo motores
+  servoCopo.attach(SERVO_COPO);
+  servoChocolate.attach(SERVO_CHOCOLATE);
+  servoLeite.attach(SERVO_LEITE);
+  servoValvula.attach(SERVO_VALVULA);
+  servoCopo.write(10);
+  servoChocolate.write(10);
+  servoLeite.write(10);
+  servoValvula.write(10);
 
+  // Mixer
+  pinMode(MIXER, OUTPUT);
+  digitalWrite(MIXER, LOW);
+
+  // Sensor e botões
+  pinMode(SENSOR_COPO, INPUT);
+  pinMode(BOTAO_CIMA, INPUT_PULLUP);
+  pinMode(BOTAO_OK, INPUT_PULLUP);
+
+  // LCD
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(0, 0);
 
-  pinMode(pinDispenseButton, INPUT);
-
-  servoMotor.setPeriodHertz(50);
-  servoMotor.attach(pinServo, 500, 2400);
-  servoMotor.write(servoPos);
-
-  WiFi.begin(ssid, password);
-  lcd.print("Conectando WiFi");
-  Serial.print("Conectando ao Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  delay(2000);
-  updateLCD();
-
-  Serial.println("\nConectado ao Wi-Fi");
-  Serial.println(WiFi.localIP());
-
-  server.on("/", handleRoot);
-  server.on("/servo", handleServo);
-  server.on("/dispenseCup", handleDispenseCup);
-  server.begin();
+  mostrarMenu();
 }
 
 void loop() {
-  server.handleClient();
-  if (digitalRead(pinDispenseButton) == LOW) {
-    handleDispenseCup();
+  if (digitalRead(BOTAO_CIMA) == LOW) {
+    intensidade = (intensidade % 3) + 1;
+    delay(200);
+    mostrarMenu();
   }
+
+  if (digitalRead(BOTAO_OK) == LOW) {
+    iniciarProcesso();
+  }
+}
+
+void mostrarMenu() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Intensidade:");
+  lcd.setCursor(0, 1);
+  lcd.print(intensidades[intensidade - 1]);
+}
+
+bool esperarCopo() {
+  for (int tentativas = 0; tentativas < 3; tentativas++) {
+    servoCopo.write(100);
+    delay(500);
+    servoCopo.write(10);
+    delay(1000);
+    if (digitalRead(SENSOR_COPO) == LOW) return true;
+  }
+  return false;
+}
+
+void dosarChocolate(int ciclos) {
+  for (int i = 0; i < ciclos; i++) {
+    servoChocolate.write(100);
+    delay(500);
+    servoChocolate.write(10);
+    delay(500);
+  }
+}
+
+void dosarLeite() {
+  servoLeite.write(100);
+  delay(2000);  // tempo fixo de vazão
+  servoLeite.write(10);
+}
+
+void iniciarProcesso() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Iniciando...");
+  delay(1000);
+
+  // Dispensar copo
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Dispensando copo");
+
+  if (!esperarCopo()) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Erro!");
+    return;
+  }
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Dose choco...");
+  dosarChocolate(intensidade);
+
+  lcd.setCursor(0, 1);
+  lcd.print("Dose leite...");
+  dosarLeite();
+
+  // Mistura
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Misturando...");
+  digitalWrite(MIXER, HIGH);
+  for (int i = TEMPO_DE_MISTURA; i > 0; i--) {
+    lcd.setCursor(0, 1);
+    lcd.print("Tempo: ");
+    lcd.print(i);
+    lcd.print("s ");
+    delay(1000);
+  }
+  digitalWrite(MIXER, LOW);
+
+  // Dispensar bebida
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Liberando bebida");
+  servoValvula.write(100);
+  delay(3000);
+  servoValvula.write(10);
+
+  // Aguarda retirada do copo
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Pronto!");
+  while (digitalRead(SENSOR_COPO) == LOW) {
+    delay(100);
+  }
+  mostrarMenu();
 }
